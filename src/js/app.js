bd4d68894bd01852d89f62414b02e5ba3d928df9
Jerome van Geuns <jerome@spotr.ai>
2022-03-31 14:24:21 +0200

    Marker and map fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,11 @@
 import { addPlace } from "../components/sidebar/sidebar.js";
 import * as L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import '../../node_modules/leaflet/dist/leaflet.css';
+import markerIconPng from '../../node_modules/leaflet/dist/images/marker-icon-2x.png';
+import markerIconPngShadow from '../../node_modules/leaflet/dist/images/marker-shadow.png';
+
 
 document.getElementById("addplace").addEventListener("click", function () { addPlace(document.getElementById("pname").value) });
-
-maplink = document.getElementById("map")
-
-// Initialize leaflet.js
-//var L = require('leaflet');
 
 // Initialize the map
 var map = L.map('map', {
@@ -16,6 +14,18 @@ var map = L.map('map', {
 
 // Set the position and zoom level of the map
 map.setView([47.70, 13.35], 7);
+
+var myIcon = L.icon({
+    iconUrl: markerIconPng,
+    iconSize: [38, 50],
+    iconAnchor: [22, 50],
+    popupAnchor: [-3, -76],
+    shadowUrl: markerIconPngShadow,
+    shadowSize: [68, 50],
+    shadowAnchor: [22, 50]
+});
+L.marker([47.70, 13.35], {icon: myIcon}).addTo(map);
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
